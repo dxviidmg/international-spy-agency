688cecddced29ea0cc73f4fa94a2f094ef9e8332
David <david@ixulabs.com>
2021-08-06 18:09:17 -0500

    users

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    status_choices =(
+        ("Active","Active"),
+        ("Retired", "Retired"),
+        ("Dead", "Dead")
+    )
+
+    type_user_choices =(
+        ("Boss","Boss"),
+        ("Manager", "Manager"),
+        ("Hitman", "Hitman")
+    )
+
+    state = models.CharField(max_length=10, choices=status_choices, default="Active")
+    type_user = models.CharField(max_length=10, choices=type_user_choices, default="Hitman")
+    boss = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
